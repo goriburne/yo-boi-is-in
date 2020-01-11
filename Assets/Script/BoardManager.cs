215b0a2f16645a9e24881bda1934a64ea12b768f
goriburne <orijosh7@yahoo.com>
2020-01-11 15:35:10 -0500

    Initial commit

---
+++ Assets/Script/BoardManager.cs
@@ -0,0 +1,255 @@
+﻿using UnityEngine;
+using System;                               //Allows us to use serializable
+using System.Collections.Generic;           //Allows us to use Lists.
+using Random = UnityEngine.Random;          //Tells Random to use the Unity Engine random number generator.
+using UnityEngine.Tilemaps;                 //Allows us to use Ruletiles and tile
+using UnityEngine.UI;                       //Allows us to use "Text"
+
+public class BoardManager : MonoBehaviour
+    {
+        // Using Serializable allows us to embed a class with sub properties in the inspector.
+        [Serializable]
+        public class Count
+        {
+            public int minimum;             //Minimum value for our Count class.
+            public int maximum;             //Maximum value for our Count class.
+
+
+            //Assignment constructor.
+            public Count(int min, int max)
+            {
+                minimum = min;
+                maximum = max;
+            }
+        }
+
+
+        public int radius = 4;                                         //radius in our game board.
+        public int xInt;                                                //Facilitates positive negave switch
+        public int scale;                                               //Log of level
+        int channelIndex = 0;                                           //Direction the channel moves next
+        public Count wallCount = new Count(5, 9);                       //Lower and upper limit for our random number of walls per level.
+        public Count foodCount = new Count(1, 5);                       //Lower and upper limit for our random number of food items per level.
+        public GameObject exit;                                         //Prefab to spawn for exit.
+        public GameObject[] floorTiles;                                 //Array of floor prefabs.
+        public GameObject[] wallTiles;                                  //Array of wall prefabs.
+        public GameObject[] foodTiles;                                  //Array of food prefabs.
+        public GameObject[] enemyTiles;                                 //Array of enemy prefabs.
+        public GameObject[] outerWallTiles;                             //Array of outer tile prefabs.
+        private Transform boardHolder;                                  //A variable to store a reference to the transform of our Board object.
+        [SerializeField] GameObject Default;                            //An Assigned TileMap for my channel rule tile
+        private List<Vector3> gridPositions = new List<Vector3>();      //A list of possible locations to place tiles.
+        private List<Vector3Int> channelDirections = new List<Vector3Int>();//Adjacent hexes directions
+        public RuleTile Channel;                                        //Imports the Gray Channel rule tile I created
+        Tilemap chMap;                                                  //A variable timemap to contain the default map
+        public Text Debug;                                              //Displays information i will ask it for on the game screen
+
+
+        public void Awake()
+        {
+            //Set chMap to Default tilemap
+            chMap = Default.GetComponent<Tilemap>();
+            channelIndex = Random.Range(0, 5);
+        
+        }
+
+        //Clears our list gridPositions, ch tilemap and prepares it to generate a new board.
+        void InitialiseList()
+        {
+            //Clear our list gridPositions.
+            gridPositions.Clear();
+            //Clear our channel tilemap.
+            chMap.RefreshAllTiles();
+
+
+            //creates a grid point at origin
+            gridPositions.Add(new Vector3(0f, 0f, 0f));
+            for (float r = 0 ; r < radius-1; r++)
+            {
+                float x = 0;
+                float y = 0;
+                y = y + .5f * r;
+                
+                for (float hexDir = 0; hexDir < 6; hexDir++)
+                {
+                    for(float steps=0; steps<r;steps++)
+                    {
+                    x =x + (1 - Mathf.Floor(hexDir / 3) * 2) * (1 - hexDir % 3)*.75f;
+                    y =y + (Mathf.Floor(hexDir / 3) * 2 - 1) * (2 - Mathf.Abs(1 - hexDir % 3))*.25f;
+                    gridPositions.Add(new Vector3(x, y, 0f));
+                    }
+                }
+            }
+            
+        channelDirections.Add(new Vector3Int(1, 0, 0));//0,1. 1,0. 1,-1. 0,-1. -1,-1. -1,0.
+        channelDirections.Add(new Vector3Int(0, 1, 0));
+        channelDirections.Add(new Vector3Int(-1, 1, 0));
+        channelDirections.Add(new Vector3Int(-1, 0, 0));
+        channelDirections.Add(new Vector3Int(-1, -1, 0));
+        channelDirections.Add(new Vector3Int(0, -1, 0));
+    }
+       
+        //Sets up the outer walls and floor (background) of the game board.
+        void BoardSetup()
+        {
+            //Instantiate Board and set boardHolder to its transform.
+            boardHolder = new GameObject("Board").transform;
+
+
+
+            GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+            GameObject instance = Instantiate(toInstantiate, new Vector3(0, 0, 0f), Quaternion.identity) as GameObject;
+            for (float r = 0; r < radius + 1; r++)
+            {
+                float x = 0;
+                float y = 0;
+
+
+                y = y + .5f * r;
+
+                for (float hexDir = 0; hexDir < 6; hexDir++)
+                {
+                    for (float steps = 0; steps < r; steps++)
+                    {
+                        toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                        if (r == radius)
+                            toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
+                        x = x + (1 - Mathf.Floor(hexDir / 3) * 2) * (1 - hexDir % 3) * .75f;
+                        y = y + (Mathf.Floor(hexDir / 3) * 2 - 1) * (2 - Mathf.Abs(1 - hexDir % 3)) * .25f;
+                        instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                        //Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+                        instance.transform.SetParent(boardHolder);
+                        //Refresh the tile map to show the new positions in the tilemap.
+                        chMap.RefreshAllTiles();
+                        //Console.WriteLine("Radius:"+r+", Direction:"+ hexDir + ", Step:" + steps + ", X Change:" + (1 - Mathf.Floor(hexDir / 3) * 2) * (1 - hexDir % 3) * .75f + ", Y Change:" + (Mathf.Floor(hexDir / 3) * 2 - 1) * (2 - Mathf.Abs(1 - hexDir % 3)) * .25f);
+                    }
+                }
+
+            }
+
+
+        }
+        
+        void ChannelDraw()
+        {
+            Color cholor = Random.ColorHSV();
+            Vector3 chPosition = RandomPosition();
+            Vector3Int chPositionInt = new Vector3Int((int)chPosition.x, (int)chPosition.y, 0);
+            chMap.SetTile(chPositionInt, Channel);
+            chMap.SetColor(chPositionInt, cholor);
+
+            for (float i = 0; i < 4+.5*scale; i++)
+            {
+                channelIndex =Random.Range((channelIndex + 2) % 6, (channelIndex + 4) % 6);
+                int read = (channelIndex + (Mathf.Abs(chPositionInt.y) % 2) * 3) % 6;
+                int tive = -1 + 2 * ((Mathf.Abs(chPositionInt.y) + 1) % 2);
+                //Debug.text = "Channel index:" + channelIndex + "chPositionInt.y:" + chPositionInt.y + "Read:" + read+ "Tive:" + tive;
+                chPositionInt = chPositionInt + channelDirections[read] *(tive);
+                //Set channel tiles *Testing*.
+                chMap.SetTile(chPositionInt, Channel);
+                chMap.SetColor(chPositionInt, cholor);
+                channelIndex = (channelIndex + 3) % 6;
+            }
+        
+        }
+        
+        //RandomPosition returns a random position from our list gridPositions.
+        Vector3 RandomPosition()
+        {
+            //Declare an integer randomIndex, set it's value to a random number between 0 and the count of items in our List gridPositions.
+            int randomIndex = Random.Range(0, gridPositions.Count);
+
+            //Declare a variable of type Vector3 called randomPosition, set it's value to the entry at randomIndex from our List gridPositions.
+            Vector3 randomPosition = gridPositions[randomIndex];
+
+            //Remove the entry at randomIndex from the list so that it can't be re-used.
+            gridPositions.RemoveAt(randomIndex);
+
+            //Return the randomly selected Vector3 position.
+            return randomPosition;
+        }
+
+
+        //LayoutObjectAtRandom accepts an array of game objects to choose from along with a minimum and maximum range for the number of objects to create.
+        void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
+        {
+            //Choose a random number of objects to instantiate within the minimum and maximum limits
+            int objectCount = Random.Range(minimum, maximum + 1);
+
+            //Instantiate objects until the randomly chosen limit objectCount is reached
+            for (int i = 0; i < objectCount; i++)
+            {
+                //Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
+                Vector3 randomPosition = RandomPosition();
+
+                //Choose a random tile from tileArray and assign it to tileChoice
+                GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+
+                //Instantiate tileChoice at the position returned by RandomPosition with no change in rotation
+                Instantiate(tileChoice, randomPosition, Quaternion.identity);
+            }
+        }
+
+
+        //SetupScene initializes our level and calls the previous functions to lay out the game board
+        public void SetupScene(int level)
+        {
+            //Find the gameobject called default and get its tile component
+            chMap = GameObject.Find("Default").GetComponent<Tilemap>();
+
+            //Remove all tiles on our ch tile map
+            chMap.ClearAllTiles();
+
+            //Scale is a variable that affects Boardsize, Ashe and Enemy Counts;
+            scale = (int)Mathf.Log(level, 2f);
+
+            //Number of radius in our game board.
+            radius = 4+ scale;                                             
+
+
+            //Creates the outer walls and floor.
+            wallCount = new Count(3+ scale, 5 + scale) ;
+
+            //Lower and upper limit for our random number of walls per level.
+            foodCount = new Count(1+ scale, 5 + scale) ;
+
+            //Reset our list of gridpositions.
+            InitialiseList();
+
+            //Sets up the outer walls and floor (background) of the game board.
+            BoardSetup();
+
+            for (int i=0;i<2+scale;i++)
+            ChannelDraw();
+
+
+            //Instantiate a random number of wall tiles based on minimum and maximum, at randomized positions.
+            LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximum);
+
+            //Instantiate a random number of food tiles based on minimum and maximum, at randomized positions.
+            LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximum);
+
+            //Determine number of enemies based on current level number, based on a logarithmic progression
+            int enemyCount = +scale;
+
+            //Instantiate a random number of enemies based on minimum and maximum, at randomized positions.
+            LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
+            //Instantiate the exit tile in the upper right hand corner of our game board
+            if (Random.Range(-1f, 1f) > ((radius+1) /(radius* 3f)))
+            {
+            Instantiate(exit,new Vector3((-1 + 2 * (int)Random.Range(0, 2)) * (radius - 1) * .75f,(int)Random.Range(0, radius/2) * .5f- (((radius+1) % 2))*.25f,0f),Quaternion.identity);
+
+
+            }
+            else
+            {
+            int c = (int)Random.Range(0, radius - 1);
+            Instantiate(exit, new Vector3(
+                (-1 + 2 * (int)Random.Range(0, 2)) * c * .75f,
+                (-1 + 2 * (int)Random.Range(0, 2))* ((radius-1)*.5f-c * .25f),
+                0f), Quaternion.identity);
+
+            }
+
+    }
+    }
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,361 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;        //Allows us to use SceneManager
+using UnityEngine.UI;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+
+//Player inherits from MovingObject, our base class for objects that can move, Enemy also inherits from this.
+public class Player : MovingObject
+{
+    public float restartLevelDelay = 1f;        //Delay time in seconds to restart level.
+    public int pointsPerFood = 10;                //Number of points to add to player food points when picking up a food object.
+    public int pointsPerSoda = 20;                //Number of points to add to player food points when picking up a soda object.
+    public int wallDamage = 1;                    //How much damage a player does to a wall when chopping it.
+    public Text foodText;                           //What the energy is displayed as
+    public Text angle;                           //What the energy is displayed as
+    public AudioClip moveSound1;                    //Footstep sound effects
+    public AudioClip moveSound2;                
+    public AudioClip eatSound1;                     //eating soundeffects
+    public AudioClip eatSound2;
+    public AudioClip drinkSound1;                   //drinking soundeffects
+    public AudioClip drinkSound2;
+    public AudioClip GameOverSound;                 //gameover soundeffects
+    public AudioClip doe;
+    public AudioClip ree;
+    public AudioClip mee;
+    public AudioClip channelStart;
+    
+
+    private Animator animator;                    //Used to store a reference to the Player's animator component.
+    private int food;                            //Used to store player food points total during level.
+
+    [SerializeField] GameObject Ashe;               //Connect the Ashe tilemap
+    [SerializeField] GameObject worldLight;         //Connect the World Light
+    Tilemap asheMap;                                //The variable to operate with our Ashe Tile map
+    private List<Vector3Int> channeledTiles = new List<Vector3Int>();      //A list of possible locations to place tiles.
+    [SerializeField] Tile lightTile;                //Connect the tile that appears while the charater is channeling
+    Vector3 tail = new Vector3(0f, 0f, 0f);
+    Vector3 head = new Vector3(0f, 0f, 0f);
+    float angleBetween;
+    // Make a game object
+    GameObject lightGameObject;
+    // Add the light component
+    Light lightComp ;
+    private Transform lightHolder;                                    //A variable to store a reference to the transform of our Board object.
+    bool channelIsIntended;
+
+    public void Awake()
+    {
+        //Connect the Game object with the variable
+        asheMap = Ashe.GetComponent<Tilemap>();
+    }
+
+    //Start overrides the Start function of MovingObject
+    protected override void Start()
+    {
+        //Get a component reference to the Player's animator component
+        animator = GetComponent<Animator>();
+
+        foodText.text = "Food:" + food;
+        //Get the current food point total stored in GameManager.instance between levels.
+        food = GameManager.instance.playerFoodPoints;
+
+        //Call the Start function of the MovingObject base class.
+        base.Start();
+    }
+
+
+    //This function is called when the behaviour becomes disabled or inactive.
+    private void OnDisable()
+    {
+        //When Player object is disabled, store the current local food total in the GameManager so it can be re-loaded in next level.
+        GameManager.instance.playerFoodPoints = food;
+    }
+
+
+    private void Update()
+    {
+        //If it's not the player's turn, exit the function.
+        if (!GameManager.instance.playersTurn) return;
+
+        float horizontal = 0;      //Used to store the horizontal move direction.
+        float vertical = 0;        //Used to store the vertical move direction.
+        
+
+
+        //Controls for movenent the map
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            horizontal = 0f;
+            vertical = 0.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            horizontal = 0.75f;
+            vertical = 0.25f;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            horizontal = 0.75f;
+            vertical = -0.25f;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            horizontal = 0;
+            vertical = -0.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            horizontal = -0.75f;
+            vertical = -0.25f;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            horizontal = -0.75f;
+            vertical = 0.25f;
+        }
+
+        //Find the position of the Player on the cell by its global postion
+        Vector3Int cellPosition = asheMap.WorldToCell(transform.localPosition);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            channeledTiles.Add(cellPosition);
+            // Make a game object
+            lightGameObject = new GameObject("TheLight");
+
+
+            // Add the light component
+            lightComp = lightGameObject.AddComponent<Light>();
+
+            // Set color, intensity,range and tag of the light
+            lightComp.color = Color.blue;
+            lightComp.intensity = 10;
+            lightComp.range = 1;
+            lightComp.tag = "Light";
+
+            // Set the position of the light (or any transform property)
+            lightGameObject.transform.position = transform.localPosition + new Vector3(0, 0, -.3f);
+
+            //The player Very Obviously means to channel
+            channelIsIntended = true;
+
+            SoundManager.instance.musicSource.volume = .005f;
+            SoundManager.instance.PlaySingle(channelStart);
+        }
+            if (Input.GetKey(KeyCode.Space)&& (channeledTiles.Count<4)&& (channelIsIntended))
+        {   //Instantiate Board and set boardHolder to its transform.
+            
+            asheMap.SetTile(cellPosition, lightTile);
+            worldLight.SetActive(false);
+            if (horizontal != 0 || vertical != 0)
+            {
+                channeledTiles.Add(cellPosition);
+                // Make a game object
+                lightGameObject = new GameObject("TheLight");
+
+
+                // Add the light component
+                lightComp = lightGameObject.AddComponent<Light>();
+
+                // Set color, intensity,range and tag of the light
+                lightComp.color = Color.blue;
+                lightComp.intensity = 10;
+                lightComp.range = 1;
+                lightComp.tag = "Light";
+
+                // Set the position of the light (or any transform property)
+                lightGameObject.transform.position = transform.localPosition + new Vector3(horizontal, vertical, -.3f);
+                
+            }
+        }
+        else
+        {
+            //Reset the channeled tiles and world light
+            channeledTiles.Clear();
+            asheMap.ClearAllTiles();
+            worldLight.SetActive(true);
+            SoundManager.instance.musicSource.volume = .15f;
+
+
+            // Function for clearing the channel light
+            GameObject[] respawns; respawns = GameObject.FindGameObjectsWithTag("Light");
+            for (int i = 0; i < respawns.Length; i++)
+            {
+                GameObject.Destroy(respawns[i]);
+            }
+            channelIsIntended = false;
+        }
+        SoundManager.instance.efxSource.volume = 1f;
+        if (channeledTiles.Count == 2)
+        {
+            SoundManager.instance.efxSource.volume = .3f;
+        }
+        else if (channeledTiles.Count == 3)
+        {
+            SoundManager.instance.efxSource.volume = 1f;
+        }
+        //Check if we have a non-zero value for horizontal or vertical
+        if (horizontal != 0 || vertical != 0)
+        {
+            head = new Vector3(horizontal, vertical, 0);
+            // angleBetween is approximately equal to 0.9548
+            angleBetween = Vector3.Angle(tail, head);
+            angle.text = "Angle:" + Mathf.Ceil(angleBetween);
+            tail = head;
+            //Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
+            //Pass in horizontal and vertical as parameters to specify the direction to move Player in.\
+            animator.SetTrigger("movingT");
+            AttemptMove<Wall>(horizontal, vertical);
+            animator.SetFloat("moveX", horizontal);
+            animator.SetFloat("moveY", vertical);
+            
+        }
+        
+
+    }
+        
+
+    //AttemptMove overrides the AttemptMove function in the base class MovingObject
+    //AttemptMove takes a generic parameter T which for Player will be of the type Wall, it also takes integers for x and y direction to move in.
+    protected override void AttemptMove<T>(float xDir, float yDir)
+    {
+        //Every time player moves, subtract from food points total.
+        food--;
+        foodText.text = "Food:" + food;
+        //Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
+        base.AttemptMove<T>(xDir, yDir);
+
+        //Hit allows us to reference the result of the Linecast done in Move.
+        RaycastHit2D hit;
+
+        //If Move returns true, meaning Player was able to move into an empty space.
+        if (Move(xDir, yDir, out hit))
+        {
+            if (channeledTiles.Count < 4&& 0 < channeledTiles.Count)
+            {
+
+
+
+                if (angleBetween == 0f || angleBetween == 180f)
+                {
+                    SoundManager.instance.PlaySingle(doe);
+                }
+                else if (angleBetween <90f)
+                {
+                    SoundManager.instance.PlaySingle(ree);
+                }
+                else if (angleBetween > 90f )
+                {
+                    SoundManager.instance.PlaySingle(mee);
+                }
+            }
+            else
+            {
+                //Call RandomizeSfx of SoundManager to play the move sound, passing in two audio clips to choose from.
+                SoundManager.instance.RandomizeSfx(moveSound1, moveSound2);
+            }
+        }
+
+        //Since the player has moved and lost food points, check if the game has ended.
+        CheckIfGameOver();
+
+        //Set the playersTurn boolean of GameManager to false now that players turn is over.
+        GameManager.instance.playersTurn = false;
+    }
+
+
+    //OnCantMove overrides the abstract function OnCantMove in MovingObject.
+    //It takes a generic parameter T which in the case of Player is a Wall which the player can attack and destroy.
+    protected override void OnCantMove<T>(T component)
+    {
+        //Set hitWall to equal the component passed in as a parameter.
+        Wall hitWall = component as Wall;
+
+        //Call the DamageWall function of the Wall we are hitting.
+        hitWall.DamageWall(wallDamage);
+
+        //Set the attack trigger of the player's animation controller in order to play the player's attack animation.
+        animator.SetTrigger("PlayerChop");
+    }
+
+
+    //OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check if the tag of the trigger collided with is Exit.
+        if (other.tag == "Exit")
+        {
+            //Invoke the Restart function to start the next level with a delay of restartLevelDelay (default 1 second).
+            Invoke("Restart", restartLevelDelay);
+
+            //Disable the player object since level is over.
+            enabled = false;
+        }
+
+        //Check if the tag of the trigger collided with is Food.
+        else if (other.tag == "Food")
+        {
+            //Add pointsPerFood to the players current food total.
+            food += pointsPerFood;
+            foodText.text = "Food:" + food;
+            SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
+            //Disable the food object the player collided with.
+            other.gameObject.SetActive(false);
+        }
+
+        //Check if the tag of the trigger collided with is Soda.
+        else if (other.tag == "Soda")
+        {
+            //Add pointsPerSoda to players food points total
+            food += pointsPerSoda;
+            foodText.text = "Food:" + food;
+            SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
+            //Disable the soda object the player collided with.
+            other.gameObject.SetActive(false);
+        }
+    }
+
+
+    //Restart reloads the scene when called.
+    private void Restart()
+    {
+        //Load the last scene loaded, in this case Main, the only scene in the game.
+        SceneManager.LoadScene(0);
+    }
+
+
+    //LoseFood is called when an enemy attacks the player.
+    //It takes a parameter loss which specifies how many points to lose.
+    public void LoseFood(int loss)
+    {
+        //Set the trigger for the player animator to transition to the PlayerHurt animation.
+        animator.SetTrigger("PlayerHurt");
+
+        //Subtract lost food points from the players total.
+        food -= loss;
+        foodText.text = "Food:" + food;
+
+        //Check to see if game has ended.
+        CheckIfGameOver();
+    }
+
+
+    //CheckIfGameOver checks if the player is out of food points and if so, ends the game.
+    private void CheckIfGameOver()
+    {
+        //Check if food point total is less than or equal to zero.
+        if (food <= 0)
+        {
+            SoundManager.instance.PlaySingle(GameOverSound);
+            SoundManager.instance.musicSource.Stop();
+            //Call the GameOver function of GameManager.
+            GameManager.instance.GameOver();
+        }
+    }
+}
+
+
